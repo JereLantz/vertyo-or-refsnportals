4b98f360d35ff1628f3e9bae758a3dc8aa97f9c1
Jere Lantz <lantz.jere@gmail.com>
2025-03-19 22:37:06 +0200

    The challenge now displays the time remaining and target time when the player didn't lose

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,13 @@
 import { forwardRef, useImperativeHandle, useRef} from "react"
 
 //export default function ResultModal({ref, result, targetTime}){
-const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref){
+const ResultModal = forwardRef(function ResultModal({onReset, targetTime, remainingTime}, ref){
     //NOTE: toi ref:in tuominen tähän komponenttiin toimii vain versiossa 19+
     // vanhemmissa joudutaan käyttämään tota importattua elementtiä reaktista
     
+    const userLost = remainingTime <= 0
+    const formattedRTime = (remainingTime/1000).toFixed(2)
+
     const dialog = useRef()
     useImperativeHandle(ref, ()=>{
         return {
@@ -15,11 +18,11 @@ const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref){
     })
     return(
         <dialog ref={dialog} className="result-modal">
-        <h2>You {result}</h2>
+        {userLost &&<h2>You lost</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
-        <p>You stopped the timer with <strong>X seconds left.</strong></p>
+        <p>You stopped the timer with <strong>{formattedRTime} seconds left.</strong></p>
 
-        <form method="dialog">
+        <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
         </form>
         </dialog>
