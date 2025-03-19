e5299cbedf1bc6785d488054635462c166093cf3
Jere Lantz <lantz.jere@gmail.com>
2025-03-19 21:08:30 +0200

    with useImperative, made the modal more independent

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,11 +1,20 @@
-import { forwardRef } from "react"
+import { forwardRef, useImperativeHandle, useRef} from "react"
 
 //export default function ResultModal({ref, result, targetTime}){
 const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref){
     //NOTE: toi ref:in tuominen tähän komponenttiin toimii vain versiossa 19+
     // vanhemmissa joudutaan käyttämään tota importattua elementtiä reaktista
+    
+    const dialog = useRef()
+    useImperativeHandle(ref, ()=>{
+        return {
+            open(){
+                dialog.current.showModal()
+            }
+        }
+    })
     return(
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={dialog} className="result-modal">
         <h2>You {result}</h2>
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
         <p>You stopped the timer with <strong>X seconds left.</strong></p>
